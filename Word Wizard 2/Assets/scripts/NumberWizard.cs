8a94b06d5804c7617ce69ced4b99d6041a59a7c3
dstrauss <fmdenver@gmail.com>
2018-11-26 19:53:41 -0500

    final commit

---
+++ Word Wizard 2/Assets/scripts/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessText;
+    
+    int guess;
+    List<int> guesses;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+    public void OnPressLower()
+    {
+        max = guess-1;
+        NextGuess();
+    }
+
+    public void NextGuess()
+    {
+        guess = Random.Range(min, max);
+        guessText.text = guess.ToString();
+    }
+}
